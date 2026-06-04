e17a13d32922a518cbe7ecddfcc5885a06a576fe
10Yash10 <91713101+10Yash10@users.noreply.github.com>
2026-06-04 17:30:23 +0530

    added get meta data of url service, get analytics summary service, deactivate url.

--- url shortner/src/modules/url/controller/UrlController.js
+++ url shortner/src/modules/url/controller/UrlController.js
@@ -28,12 +28,32 @@ export const handleRedirect = asyncHandler(async (req, res) => {
     referrer: req.headers["referer"],
   };
 
-  console.log("handle REdirect controller: ", requestDetails);
-
   const destinationUrl = await UrlService.resolveAndProcessRedirect(
     shortCode,
     requestDetails,
   );
 
   return res.redirect(302, destinationUrl);
+});
+
+export const getUrlDetails = asyncHandler(async (req, res) => {
+  const { shortCode } = req.params;
+
+  const metadata = await UrlService.getUrlMetadata(shortCode);
+
+  return res.status(200).json({ status: "success", data: metadata });
+});
+
+export const getAnalyticsSummary = asyncHandler(async (req, res) => {
+  const metrics = await UrlService.getAggregatedMetrics(req.params.shortCode);
+
+  return res.status(200).json({ status: "success", data: metrics });
+});
+
+export const deactivateUrl = asyncHandler(async (req, res) => {
+  const confirmation = await UrlService.deactivateUrlService(
+    req.params.shortCode,
+  );
+
+  return res.status(200).json({ status: "success", data: confirmation });
 });
