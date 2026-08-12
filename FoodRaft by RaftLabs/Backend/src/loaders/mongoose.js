9b43e042a64c55af9e3184926c4416d496a14ab7
10Yash10 <91713101+10Yash10@users.noreply.github.com>
2026-08-12 13:02:15 +0530

    adding a new project for a food delivery application called as FoodRaft by RaftLabs

---
+++ FoodRaft by RaftLabs/Backend/src/loaders/mongoose.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { config } from "../config/env.js";
+
+export default async () => {
+  try {
+    mongoose.connect(config.MONGO_URI, { dbName: "FoodRaft" });
+    console.log("connected to db successfully");
+  } catch (err) {
+    console.log("unable to connect to db because: ", err);
+  }
+};
